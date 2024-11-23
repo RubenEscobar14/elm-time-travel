ad6784cfdad4bdae055524c250c49710d2b2b0b5
RubenEscobar14 <ruben.escobar.c14@gmail.com>
2024-11-23 15:44:29 -0600

    Added history bar, but it's a nasty green

--- src/TimeTravel.elm
+++ src/TimeTravel.elm
@@ -4,13 +4,26 @@ import Playground exposing (..)
 import Set
 import Dict exposing (update)
 
+controlBarHeight : number
 controlBarHeight = 64
+maxVisibleHistory = 2000
 
 initialStateWithTimeTravel rawGame =
-    {rawModel = rawGame.initialState, paused = False}
+    {rawModel = rawGame.initialState, paused = False, history = []}
 
 viewWithTimeTravel rawGame computer model =
   let
+    histLength = List.length model.history
+    -- Creates a rectangle at the top of the screen, stretching from the
+    -- left edge up to a specific position within the history timeline
+    historyBar color opacity index =
+      let
+        width = historyIndexToX computer index
+      in
+        rectangle color width controlBarHeight
+          |> move (computer.screen.left + width/2)
+                  (computer.screen.top - controlBarHeight/2)
+          |> fade opacity
     helpMessage =
       if model.paused then
         "Press R to resume"
@@ -18,7 +31,9 @@ viewWithTimeTravel rawGame computer model =
         "Press T to time travel"
   in
     (rawGame.view computer model.rawModel) ++
-      [ words white helpMessage
+      [ historyBar black 0.3 maxVisibleHistory
+      , historyBar green 0.6 histLength
+      , words white helpMessage
           |> move 0 (computer.screen.top - controlBarHeight / 2)
       ]
 
@@ -26,11 +41,15 @@ updateWithTimeTravel rawGame computer model =
   if keyPressed "T" computer then
     { model | paused = True}
   else if keyPressed "R" computer then
-    { model | paused = False, rawModel = rawGame.updateState computer model.rawModel}
+    { model | paused = False, rawModel = rawGame.updateState computer model.rawModel
+    , history = model.history ++ [computer]
+    }
   else if model.paused then
     model
   else
-    { model | rawModel = rawGame.updateState computer model.rawModel }
+    { model | rawModel = rawGame.updateState computer model.rawModel
+    , history = model.history ++ [computer]
+    }
 
 addTimeTravel rawGame =
   { initialState = initialStateWithTimeTravel rawGame
@@ -38,8 +57,13 @@ addTimeTravel rawGame =
   , view = viewWithTimeTravel rawGame
   }
 
+-- Helper functions 
 keyPressed keyName computer = 
   [ String.toLower keyName
   , String.toUpper keyName
   ]
     |> List.any (\key -> Set.member key computer.keyboard.keys)
+
+-- Converts an index in the history list to an x coordinate on the screen
+historyIndexToX computer index =
+  (toFloat index) / maxVisibleHistory * computer.screen.width
