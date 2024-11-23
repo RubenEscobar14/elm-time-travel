3bda60b20cc116ea538bc119f57e14c76f7a2596
RubenEscobar14 <ruben.escobar.c14@gmail.com>
2024-11-23 13:45:31 -0600

    Set up time travel framework, changed nothing

--- src/TimeTravel.elm
+++ src/TimeTravel.elm
@@ -2,4 +2,19 @@ module TimeTravel exposing (addTimeTravel)
 
 import Playground exposing (..)
 import Set
+import Dict exposing (update)
 
+initialStateWithTimeTravel rawGame =
+    rawGame.initialState
+
+viewWithTimeTravel rawGame computer model =
+  rawGame.view computer model
+
+updateWithTimeTravel rawGame computer model =
+  rawGame.updateState computer model
+
+addTimeTravel rawGame =
+  { initialState = initialStateWithTimeTravel rawGame
+  , updateState = updateWithTimeTravel rawGame
+  , view = viewWithTimeTravel rawGame
+  }
